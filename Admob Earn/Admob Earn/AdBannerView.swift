ce08624fd7955d358abbcff7ee6d19a54867f6e6
cdapayne <carlton.payne@hotmail.com>
2025-09-14 17:34:51 -0400

    iupdate

--- Admob Earn/Admob Earn/AdBannerView.swift
+++ Admob Earn/Admob Earn/AdBannerView.swift
@@ -4,15 +4,15 @@ import GoogleMobileAds
 struct AdBannerView: UIViewRepresentable {
     var adUnitId: String
 
-    func makeUIView(context: Context) -> GADBannerView {
-        let banner = GADBannerView(adSize: GADAdSizeBanner)
+    func makeUIView(context: Context) -> BannerView {
+        let banner = BannerView(adSize: AdSizeBanner)
         banner.adUnitID = adUnitId
-        banner.load(GADRequest())
+        banner.load(Request())
         banner.rootViewController = UIApplication.shared.connectedScenes
             .compactMap { ($0 as? UIWindowScene)?.keyWindow?.rootViewController }
             .first
         return banner
     }
 
-    func updateUIView(_ uiView: GADBannerView, context: Context) {}
+    func updateUIView(_ uiView: BannerView, context: Context) {}
 }
