21310c67da33569003540bc70ec103686e94a498
cdapayne <carlton.payne@hotmail.com>
2025-09-14 17:42:23 -0400

    Remove banner ads

--- Admob Earn/Admob Earn/Admob_EarnApp.swift
+++ Admob Earn/Admob Earn/Admob_EarnApp.swift
@@ -1,11 +1,7 @@
 import SwiftUI
-import GoogleMobileAds
 
 @main
 struct Admob_EarnApp: App {
-    init() {
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-    }
 
     var body: some Scene {
         WindowGroup {
