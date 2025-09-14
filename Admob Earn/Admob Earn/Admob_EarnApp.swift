8e15f561282493b462ec81108a764a4a12cf971f
cdapayne <carlton.payne@hotmail.com>
2025-09-14 11:32:17 -0400

    update

---
+++ Admob Earn/Admob Earn/Admob_EarnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Admob_EarnApp.swift
+//  Admob Earn
+//
+//  Created by Backup Admin on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Admob_EarnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Admob Earn/Admob EarnTests/Admob_EarnTests.swift
@@ -0,0 +1,16 @@
+//
+//  Admob_EarnTests.swift
+//  Admob EarnTests
+//
+//  Created by Backup Admin on 9/14/25.
+//
+
+import Testing
+
+struct Admob_EarnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
